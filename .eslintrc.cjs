0a5fd66477b539c06ec2e6ecd43be25ea3d6de9d
pwv2 <pwv2@outlook.com>
2024-06-27 17:23:47 +0800

    Update lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off"
   },
   parserOptions: {
     ecmaVersion: "latest",
